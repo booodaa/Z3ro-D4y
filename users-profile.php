56b6ff4f2ee24626ac03fb4688c9857e7fb0e7bd
booodaa <101272412+booodaa@users.noreply.github.com>
2023-11-12 20:55:02 +0200

    Update users-profile.php

--- users-profile.php
+++ users-profile.php
@@ -55,7 +55,7 @@ if (!isset($_SESSION['Clint_name'])) {
           </a>
         </li>
         <li class="nav-item dropdown">
-          
+
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
 
             <li>
@@ -198,10 +198,16 @@ if (!isset($_SESSION['Clint_name'])) {
                 <div class="tab-pane fade show active profile-overview" id="profile-overview">
                   <h5 class="card-title">Profile Details</h5>
 
-                    <div class="row">
+                  <div class="row">
                     <div class="col-lg-3 col-md-4 label">ID</div>
-                    <div class="col-lg-9 col-md-8"></div>
+                    <div class="col-lg-9 col-md-8"><?php echo $_SESSION['User_ID']; ?></div>
                   </div>
+
+                  <div class="row">
+                    <div class="col-lg-3 col-md-4 label">User Name</div>
+                    <div class="col-lg-9 col-md-8"><?php echo $_SESSION['User_name']; ?></div>
+                  </div>
+
                   <div class="row">
                     <div class="col-lg-3 col-md-4 label ">Full Name</div>
                     <div class="col-lg-9 col-md-8"><?php echo $_SESSION['Clint_name']; ?></div>
@@ -210,7 +216,7 @@ if (!isset($_SESSION['Clint_name'])) {
                     <div class="col-lg-3 col-md-4 label">Email</div>
                     <div class="col-lg-9 col-md-8"><?php echo $_SESSION['Email']; ?></div>
                   </div>
-                
+
 
                 </div>
 
