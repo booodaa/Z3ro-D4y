fd7d9c63d0f350c6a195278845b1f34f9be2af34
booodaa <101272412+booodaa@users.noreply.github.com>
2023-11-12 17:59:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -30,16 +30,10 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
   <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
   <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">     <link href="assets/css/style.css" rel="stylesheet">
 </head>
 
-<body>
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top d-flex align-items-center">
+<body>     <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.php" class="logo d-flex align-items-center">
@@ -47,20 +41,14 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
         <span class="d-none d-lg-block">Z3ro D4y</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
-    </div><!-- End Logo -->
-
-
-
-    <nav class="header-nav ms-auto">
+    </div>       <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
 
         <li class="nav-item d-block d-lg-none">
           <a class="nav-link nav-icon search-bar-toggle " href="#">
             <i class="bi bi-search"></i>
           </a>
-        </li><!-- End Search Icon-->
-
-        <li class="nav-item dropdown">
+        </li>           <li class="nav-item dropdown">
 
 
 
@@ -128,11 +116,7 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
               <a href="#">Show all notifications</a>
             </li>
 
-          </ul><!-- End Notification Dropdown Items -->
-
-        </li><!-- End Notification Nav -->
-
-        <li class="nav-item dropdown">
+          </ul>           </li>           <li class="nav-item dropdown">
 
 
 
@@ -191,19 +175,13 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
               <a href="#">Show all messages</a>
             </li>
 
-          </ul><!-- End Messages Dropdown Items -->
-
-        </li><!-- End Messages Nav -->
-
-        <li class="nav-item dropdown pe-3">
+          </ul>           </li>           <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
 
             <img src="img/profile.png" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo $_SESSION['Clint_name']; ?></span>
-          </a><!-- End Profile Iamge Icon -->
-
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+          </a>             <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
               <h6><?php echo $_SESSION['Clint_name']; ?></h6>
 
@@ -246,16 +224,8 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
               </a>
             </li>
 
-          </ul><!-- End Profile Dropdown Items -->
-        </li><!-- End Profile Nav -->
-
-      </ul>
-    </nav><!-- End Icons Navigation -->
-
-  </header><!-- End Header -->
-
-  <!-- ======= Sidebar ======= -->
-  <aside id="sidebar" class="sidebar">
+          </ul>           </li>         </ul>
+    </nav>     </header>        <aside id="sidebar" class="sidebar">
 
     <ul class="sidebar-nav" id="sidebar-nav">
 
@@ -264,9 +234,7 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
-      </li><!-- End Dashboard Nav -->
-
-      <li class="nav-item">
+      </li>         <li class="nav-item">
 
         <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
@@ -296,13 +264,9 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
           <img src="img/transaction.png" alt="image load error" width="20" height="25">
           <span>Transaction</span>
         </a>
-      </li><!-- End Profile Page Nav -->
+      </li>       </ul>
 
-    </ul>
-
-  </aside><!-- End Sidebar-->
-
-  <main id="main" class="main">
+  </aside>     <main id="main" class="main">
 
     <div class="pagetitle">
       <h1>Dashboard</h1>
@@ -312,19 +276,9 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
           <li class="breadcrumb-item active">Dashboard</li>
         </ol>
       </nav>
-    </div><!-- End Page Title -->
-
-    <section class="section dashboard">
-      <div class="row">
-
-        <!-- Left side columns -->
-        <div class="col-lg-8">
-          <div class="row">
-
-
-
-            <!-- Revenue Card -->
-            <div class="col-xxl-4 col-md-6">
+    </div>       <section class="section dashboard">
+      <div class="row">           <div class="col-lg-8">
+          <div class="row">               <div class="col-xxl-4 col-md-6">
               <div class="card info-card revenue-card">
 
                 <div class="filter">
@@ -345,20 +299,13 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
                 </div>
 
               </div>
-            </div><!-- End Revenue Card -->
-
-
-            <!-- Reports -->
-            <div class="col-12">
+            </div>                  <div class="col-12">
               <div class="card">
 
 
 
                 <div class="card-body">
-                  <h5 class="card-title">Reports <span>/Today</span></h5>
-
-                  <!-- Line Chart -->
-                  <div id="reportsChart"></div>
+                  <h5 class="card-title">Reports <span>/Today</span></h5>                     <div id="reportsChart"></div>
 
                   <script>
                     document.addEventListener("DOMContentLoaded", () => {
@@ -411,16 +358,10 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
                         }
                       }).render();
                     });
-                  </script>
-                  <!-- End Line Chart -->
-
-                </div>
+                  </script>                   </div>
 
               </div>
-            </div><!-- End Reports -->
-
-            <!-- Recent Sales -->
-            <div class="col-12">
+            </div>                  <div class="col-12">
               <div class="card recent-sales overflow-auto">
 
                 <div class="filter">
@@ -439,10 +380,7 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
 
 
               </div>
-            </div><!-- End Recent Sales -->
-
-            <!-- Top Selling -->
-            <div class="col-12">
+            </div>                  <div class="col-12">
               <div class="card top-selling overflow-auto">
 
                 <div class="filter">
@@ -459,16 +397,8 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
                 </div>
 
               </div>
-            </div><!-- End Top Selling -->
-
-          </div>
-        </div><!-- End Left side columns -->
-
-        <!-- Right side columns -->
-        <div class="col-lg-4">
-
-          <!-- Recent Activity -->
-          <div class="card">
+            </div>             </div>
+        </div>              <div class="col-lg-4">             <div class="card">
             <div class="filter">
               <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
               <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
@@ -493,55 +423,40 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
                   <div class="activity-content">
                     Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae
                   </div>
-                </div><!-- End activity item-->
-
-                <div class="activity-item d-flex">
+                </div>                   <div class="activity-item d-flex">
                   <div class="activite-label">56 min</div>
                   <i class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
                   <div class="activity-content">
                     Voluptatem blanditiis blanditiis eveniet
                   </div>
-                </div><!-- End activity item-->
-
-                <div class="activity-item d-flex">
+                </div>                   <div class="activity-item d-flex">
                   <div class="activite-label">2 hrs</div>
                   <i class='bi bi-circle-fill activity-badge text-primary align-self-start'></i>
                   <div class="activity-content">
                     Voluptates corrupti molestias voluptatem
                   </div>
-                </div><!-- End activity item-->
-
-                <div class="activity-item d-flex">
+                </div>                   <div class="activity-item d-flex">
                   <div class="activite-label">1 day</div>
                   <i class='bi bi-circle-fill activity-badge text-info align-self-start'></i>
                   <div class="activity-content">
                     Tempore autem saepe <a href="#" class="fw-bold text-dark">occaecati voluptatem</a> tempore
                   </div>
-                </div><!-- End activity item-->
-
-                <div class="activity-item d-flex">
+                </div>                   <div class="activity-item d-flex">
                   <div class="activite-label">2 days</div>
                   <i class='bi bi-circle-fill activity-badge text-warning align-self-start'></i>
                   <div class="activity-content">
                     Est sit eum reiciendis exercitationem
                   </div>
-                </div><!-- End activity item-->
-
-                <div class="activity-item d-flex">
+                </div>                   <div class="activity-item d-flex">
                   <div class="activite-label">4 weeks</div>
                   <i class='bi bi-circle-fill activity-badge text-muted align-self-start'></i>
                   <div class="activity-content">
                     Dicta dolorem harum nulla eius. Ut quidem quidem sit quas
                   </div>
-                </div><!-- End activity item-->
-
-              </div>
+                </div>                 </div>
 
             </div>
-          </div><!-- End Recent Activity -->
-
-          <!-- Budget Report -->
-          <div class="card">
+          </div>                <div class="card">
             <div class="filter">
               <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
               <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
@@ -609,14 +524,7 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
             </script>
 
           </div>
-        </div><!-- End Budget Report -->
-
-        <!-- Website Traffic -->
-
-
-
-
-        <script>
+        </div>              <script>
           document.addEventListener("DOMContentLoaded", () => {
             echarts.init(document.querySelector("#trafficChart")).setOption({
               tooltip: {
@@ -672,10 +580,7 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
         </script>
 
       </div>
-      </div><!-- End Website Traffic -->
-
-      <!-- News & Updates Traffic -->
-      <div class="card">
+      </div>            <div class="card">
         <div class="filter">
           <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
@@ -689,36 +594,18 @@ if (!isset($_SESSION['Clint_name']) || empty($_SESSION['Clint_name'])) {
           </ul>
         </div>
 
-
-
-      </div><!-- End sidebar recent posts-->
-
-      </div>
-      </div><!-- End News & Updates -->
-
-      </div><!-- End Right side columns -->
-
-      </div>
+      </div>         </div>
+      </div>         </div>         </div>
     </section>
 
-  </main><!-- End #main -->
-
-
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  </main>     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>     <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/chart.js/chart.umd.js"></script>
   <script src="assets/vendor/echarts/echarts.min.js"></script>
   <script src="assets/vendor/quill/quill.min.js"></script>
   <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>     <script src="assets/js/main.js"></script>
 
 </body>
 
