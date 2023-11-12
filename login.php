552fddd74a1367ae319b47ed291d105f0b32e229
booodaa <101272412+booodaa@users.noreply.github.com>
2023-11-12 11:41:02 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,39 +3,41 @@ session_start();
 include("database.php");
 
 if (isset($_POST['username'], $_POST['password'])) {
-    $User_name = $_POST['username'];
-    $password = $_POST['password'];
+  $User_name = $_POST['username'];
+  $password = $_POST['password'];
 
-    $stmt = $conn->prepare("SELECT * FROM `users` WHERE BINARY User_name = ?");
-    $stmt->bind_param("s", $User_name);
-    $stmt->execute();
-    $result = $stmt->get_result();
+  $stmt = $conn->prepare("SELECT * FROM `users` WHERE BINARY User_name = ?");
+  $stmt->bind_param("s", $User_name);
+  $stmt->execute();
+  $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $hash = $row['Password'];
+  if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $hash = $row['Password'];
 
-        if (password_verify($password, $hash)) {
-            // Login successful
-            $_SESSION['Clint_name'] = $row['Clint_name'];
-            $_SESSION['User_name'] = $row['User_name'];
-            $_SESSION['Email'] = $row['Email'];
-            $_SESSION['Balance'] = $row['Balance'];
+    if (password_verify($password, $hash)) {
+      // Login successful
+    
+      $_SESSION['User_ID'] = $row['User_ID'];
+      $_SESSION['Clint_name'] = $row['Clint_name'];
+      $_SESSION['User_name'] = $row['User_name'];
+      $_SESSION['Email'] = $row['Email'];
+      $_SESSION['Balance'] = $row['Balance'];
 
-            echo "Login successful!";
-            header("location: index.php");
-            exit();
-        } else {
-            // Invalid password
-            echo "Invalid password!";
-        }
+      echo "Login successful!";
+      header("location: index.php");
+      exit();
     } else {
-        // User not found
-        echo "User not found!";
+      // Invalid password
+      echo "Invalid password!";
     }
+  } else {
+    // User not found
+    echo "User not found!";
+  }
 
-    $stmt->close();
-    $conn->close();
+  $stmt->close();
+  $conn->close();
 }
 
 ?>
@@ -71,7 +73,7 @@ if (isset($_POST['username'], $_POST['password'])) {
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 
- 
+
 </head>
 
 <body>
@@ -105,7 +107,7 @@ if (isset($_POST['username'], $_POST['password'])) {
                     <div class="col-12">
                       <label for="yourUsername" class="form-label">Username</label>
                       <div class="input-group has-validation">
-                       
+
                         <input type="text" name="username" class="form-control" id="yourUsername" required>
                         <div class="invalid-feedback">Please enter your username.</div>
                       </div>
