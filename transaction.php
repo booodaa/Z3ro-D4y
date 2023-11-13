6d83d981a9d1f36c08fad73d868475dfde824e8e
booodaa <101272412+booodaa@users.noreply.github.com>
2023-11-13 22:58:30 +0200

    Update transaction.php

--- transaction.php
+++ transaction.php
@@ -204,6 +204,7 @@ include('php/transaction.php');
     </div>
   </div>
 
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/js/main.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -213,17 +214,16 @@ include('php/transaction.php');
   </div>
 
 
-  <script src="assets/js/main.js"></script>
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>     
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/chart.js/chart.umd.js"></script>
   <script src="assets/vendor/echarts/echarts.min.js"></script>
   <script src="assets/vendor/quill/quill.min.js"></script>
   <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-
+   
   <script>
     function validateInputFields() {
       var isValid = true;
