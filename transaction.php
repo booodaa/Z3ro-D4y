dbd4ebaa5431d570d206cd34e226d5117a1554a4
Noursameh1212 <121134519+Noursameh1212@users.noreply.github.com>
2023-11-30 18:23:44 +0200

    Update transaction.php

--- transaction.php
+++ transaction.php
@@ -136,15 +136,15 @@ include('php/process_transfer.php');
 
   <main>
 
-    <aside id="sidebar" class="sidebar">
+     <aside id="sidebar" class="sidebar">
 
       <ul class="sidebar-nav" id="sidebar-nav">
 
         <li class="nav-item">
-          <a class="nav-link " href="index.php">
-            <i class="bi bi-grid"></i>
-            <span>Dashboard</span>
-          </a>
+        <a class="nav-link collapsed" href="index.php">
+    <i class="bi bi-grid"></i>
+      <span>Dashboard</span>
+    </a>
         </li>
 
         <li class="nav-item">
@@ -152,22 +152,22 @@ include('php/process_transfer.php');
         <li class="nav-heading">Pages</li>
 
         <li class="nav-item">
-          <a class="nav-link " href="users-profile.php">
-            <i class="bi bi-person"></i>
-            <span>Profile</span>
-          </a>
-
-          <a class="nav-link collapsed" href="transaction.php">
-            <i class="bi bi-currency-dollar"></i>
+        <a class="nav-link collapsed" href="users-profile.php">
+        <i class="bi bi-person"></i>
+          <span>Profile</span>
+        </a>
+          <a class="nav-link " href="transaction.php">
+          <i class="bi bi-currency-dollar"></i>
             <span>Transaction</span>
           </a>
 
+        
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="transaction-history.php">
-            <i class="bi bi-clock-history"></i>
-            <span>Transaction History</span>
-          </a>
+        <a class="nav-link collapsed" href="transaction-history.php">
+    <i class="bi bi-clock-history"></i>
+      <span>Transaction History</span>
+    </a>
         </li>
       </ul>
 
