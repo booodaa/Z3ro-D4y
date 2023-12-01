c5ace3b39068c8189ec932c11171126f0d536e26
Noursameh1212 <121134519+Noursameh1212@users.noreply.github.com>
2023-12-01 15:54:24 +0200

    Update transaction-history.php

--- transaction-history.php
+++ transaction-history.php
@@ -122,41 +122,51 @@ include('php/index.php');
     </header>
 
    <aside id="sidebar" class="sidebar">
-        <ul class="sidebar-nav" id="sidebar-nav">
-            <li class="nav-heading"></li>
 
-            <li class="nav-item">
-            <a class="nav-link collapsed" href="index.php">
-    <i class="bi bi-grid"></i>
-      <span>Dashboard</span>
-    </a>
-            </li>
+<ul class="sidebar-nav" id="sidebar-nav">
 
-            <li class="nav-heading">Pages</li>
+ 
 
-            <li class="nav-item " >
-            <a class="nav-link collapsed" href="users-profile.php">
-        <i class="bi bi-person"></i>
-          <span>Profile</span>
-        </a>
-            </li>
+<li class="nav-item">
 
-            <li class="nav-item">
-            <a class="nav-link collapsed" href="transaction.php">
+<a class="nav-link collapsed" href="index.php">
+<i class="bi bi-grid"></i>
+  <span>Dashboard</span>
+</a>
+</li>
+
+<li class="nav-item">
+
+
+<a class="nav-link collapsed" href="users-profile.php">
+<i class="bi bi-person"></i>
+ <span>Profile</span>
+</a>
+
+
+</li>
+
+  <li class="nav-item">
+   
+
+    <a class="nav-link collapsed" href="transaction.php">
       <i class="bi bi-currency-dollar"></i>
       <span>Transaction</span>
     </a>
-            </li>
+   
 
-            <li class="nav-item">
-                <a class="nav-link" href="transaction-history.php">
-                    <i class="bi bi-clock-history"></i>
-                    <span>Transaction History</span>
-                </a>
-            </li>
-        </ul>
-    </aside>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="transaction-history.php">
+    <i class="bi bi-clock-history"></i>
+      <span>Transaction History</span>
+    </a>
+  </li>
+  
+  
+</ul>
 
+</aside>
     <main id="main" class="main">
 
         <div class="pagetitle">
